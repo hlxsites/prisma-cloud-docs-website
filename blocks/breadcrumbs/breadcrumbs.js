3ba3ce201d7294891eb72f3947ae6166a860ae77
icaraps <icaraps@hotmail.com>
2023-05-17 18:11:03 +0400

    fix breadcrumbs on landing page

--- blocks/breadcrumbs/breadcrumbs.js
+++ blocks/breadcrumbs/breadcrumbs.js
@@ -1,4 +1,5 @@
 import { parseFragment, PATH_PREFIX } from '../../scripts/scripts.js';
+import { getMetadata } from '../../scripts/lib-franklin.js';
 
 /**
  * Loads breadcrumbs template
@@ -28,55 +29,61 @@ export default async function decorate(block) {
     const div = document.createElement('div');
     div.setAttribute('slot', 'book');
 
-    const { lang } = document.documentElement;
     const slot = template.querySelector('slot');
 
-    const path = [];
-    const pathname = window.location.pathname.replace(PATH_PREFIX, '').split('/').slice(2);
-    const additionalNavItems = pathname.map((subPath, index) => {
-      path.push(subPath);
+    if (getMetadata('template') === 'book') {
+      const { lang } = document.documentElement;
 
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      a.href = `${window.location.origin}${PATH_PREFIX}/${lang}/${path.join('/')}`;
-      li.append(a);
+      const path = [];
+      const pathname = window.location.pathname.replace(PATH_PREFIX, '').split('/').slice(2);
 
-      // Book
-      if (index === 1) {
-        a.textContent = window.book.default.data[0].title;
+      const additionalNavItems = pathname.map((subPath, index) => {
+        path.push(subPath);
+
+        const li = document.createElement('li');
+        const a = document.createElement('a');
         a.href = `${window.location.origin}${PATH_PREFIX}/${lang}/${path.join('/')}`;
+        li.append(a);
 
-        return li;
-      }
-
-      // TODO Identify version via meta
-
-      // Chapter
-      if (index === 2) {
-        const chapter = window.book.chapters.data.find(({ key }) => key === subPath);
-        if (chapter) {
-          a.textContent = chapter.name;
-          a.href = `${window.location.origin}${PATH_PREFIX}/${lang}/${path.join('/')}/${chapter.key}`;
+        // Book
+        if (index === 1) {
+          a.textContent = window.book.default.data[0].title;
+          a.href = `${window.location.origin}${PATH_PREFIX}/${lang}/${path.join('/')}`;
 
           return li;
         }
-      }
 
-      // Topic
-      if (index > 2) {
-        const topic = window.book.topics.data.find(({ key }) => key.replaceAll('_', '-') === subPath);
-        if (topic && topic.key.replaceAll('_', '-') !== topic.chapter) {
-          a.textContent = topic.name;
+        // TODO Identify version via meta
 
-          return li;
+        // Chapter
+        if (index === 2) {
+          const chapter = window.book.chapters.data.find(({ key }) => key === subPath);
+          if (chapter) {
+            a.textContent = chapter.name;
+            a.href = `${window.location.origin}${PATH_PREFIX}/${lang}/${path.join('/')}/${chapter.key}`;
+
+            return li;
+          }
         }
-      }
 
-      return null;
-    })
-      .filter((el) => el !== null);
+        // Topic
+        if (index > 2) {
+          const topic = window.book.topics.data.find(({ key }) => key.replaceAll('_', '-') === subPath);
+          if (topic && topic.key.replaceAll('_', '-') !== topic.chapter) {
+            a.textContent = topic.name;
 
-    slot.replaceWith(...additionalNavItems);
+            return li;
+          }
+        }
+
+        return null;
+      })
+        .filter((el) => el !== null);
+
+      slot.replaceWith(...additionalNavItems);
+    } else {
+      slot.remove();
+    }
 
     // locale
     const locale = window.placeholders[`${PATH_PREFIX}/${document.documentElement.lang}`];
