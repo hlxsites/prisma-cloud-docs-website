e0abddb733e504adb025a381c1017b6ac01eaf67
Max Edell <maxed@adobe.com>
2023-06-01 15:29:35 -0700

    add procedure block, fontawesome icons

---
+++ blocks/procedure/procedure.js
@@ -0,0 +1,24 @@
+/**
+ * @param {HTMLDivElement} block
+ */
+export default function decorate(block) {
+  const lists = block.querySelectorAll(':scope > div > ol');
+  lists.forEach((list) => {
+    list.classList.add('steps');
+
+    const steps = list.querySelectorAll(':scope > li');
+    steps.forEach((step) => {
+      step.classList.add('step');
+
+      const sublists = step.querySelectorAll(':scope > ol');
+      sublists.forEach((sublist) => {
+        sublist.classList.add('substeps');
+
+        const substeps = sublist.querySelectorAll(':scope > li');
+        substeps.forEach((substep) => {
+          substep.classList.add('substep');
+        });
+      });
+    });
+  });
+}
