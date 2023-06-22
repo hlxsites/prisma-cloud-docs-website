64c97a5c8fb5b73b5daef13454af4b91772915b5
Max Edell <maxed@adobe.com>
2023-06-22 15:04:49 -0700

    rm loadArticle from scripts for now

--- prisma/prisma-cloud/scripts/scripts.js
+++ prisma/prisma-cloud/scripts/scripts.js
@@ -477,44 +477,6 @@ export async function loadBook(href) {
 }
 
 /**
- * Load article as HTML
- * @param {string} href
- * @returns {Promise<{ok:boolean;data?:string;status:number;}>}
- */
-export async function loadArticle(href) {
-  assertValidDocsURL(href);
-
-  // change href to point to docs origin on lower envs
-  if (store.env !== 'prod' && href.startsWith('/')) {
-    // eslint-disable-next-line no-param-reassign
-    href = `${DOCS_ORIGINS[store.env]}${href}`;
-  }
-
-  const url = new URL(`${href}.plain.html`);
-  setBranch(url);
-
-  const resp = await fetch(url.toString(), store.branch ? { cache: 'reload' } : undefined);
-  if (!resp.ok) return resp;
-  try {
-    const lastModified = resp.headers.get('last-modified') !== 'null' ? new Date(resp.headers.get('last-modified')) : new Date();
-    return {
-      ok: true,
-      status: resp.status,
-      info: {
-        lastModified,
-      },
-      data: await resp.text(),
-    };
-  } catch (e) {
-    console.error('failed to parse article: ', e);
-    return {
-      ...resp,
-      ok: false,
-    };
-  }
-}
-
-/**
  * Builds hero block and prepends to main in a new section.
  * @param {Element} main The container element
  */
