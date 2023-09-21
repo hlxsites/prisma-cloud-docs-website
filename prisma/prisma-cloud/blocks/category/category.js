96ea46e2a2b6f375fba19d2403dbd96c581c5c3c
Charlie Boles <charlie@charlieboles.com>
2023-09-21 12:04:46 -0700

    fix: update accordion design layout

--- prisma/prisma-cloud/blocks/category/category.js
+++ prisma/prisma-cloud/blocks/category/category.js
@@ -5,19 +5,19 @@ import {
 
 const TEMPLATE_BUTTON = `
 <div slot="category-button" class="ops-accordion-item">
-  <div class="ops-icon-button"> 
-  <svg class="icon" width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <g id="Group 230">
-    <line id="Line 83" x1="6.25" y1="11.25" x2="6.25" y2="0.75" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
-    <line id="Line 84" x1="0.75" y1="6.25" x2="11.25" y2="6.25" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
-    </g>
-  </svg>
-  </div>
   <button class="summary">
     <span class="eyebrow">Use Case</span>
     <slot name="teaser"></slot>
   </button>
   <slot name="details" class="accordion-details"></slot>
+  <div class="ops-accordion-item-toggle">
+    <span class="ops-accordion-item-toggle-expand">See more outcomes</span>
+    <span class="ops-accordion-item-toggle-close">Close more outcomes</span>
+    <svg focusable="false" aria-label="Clear" class="icon icon-down-arrow" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+      <title>Down Arrow</title>
+      <path d="M7.79 9.671c-0.867-0.894-2.276-0.894-3.144 0-0.862 0.889-0.862 2.327 0 3.217l8.717 8.988c1.455 1.5 3.817 1.5 5.272 0l8.717-8.988c0.862-0.889 0.862-2.327 0-3.217-0.867-0.894-2.276-0.894-3.144 0l-7.492 7.724c-0.393 0.405-1.043 0.405-1.436 0l-7.492-7.724z"></path>
+    </svg>
+  </div>
 </div>
 `;
 
