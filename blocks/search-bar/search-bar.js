9ce7428a6ff13c30c2d22b679b6be532bd2b510f
Max Edell <maxed@adobe.com>
2023-10-18 13:26:37 -0700

    fix: add robots.txt, change header level order, add hrefs

--- blocks/search-bar/search-bar.js
+++ blocks/search-bar/search-bar.js
@@ -14,9 +14,9 @@ const TEMPLATE = /* html */ `
       <path d="M7.79 9.671c-0.867-0.894-2.276-0.894-3.144 0-0.862 0.889-0.862 2.327 0 3.217l8.717 8.988c1.455 1.5 3.817 1.5 5.272 0l8.717-8.988c0.862-0.889 0.862-2.327 0-3.217-0.867-0.894-2.276-0.894-3.144 0l-7.492 7.724c-0.393 0.405-1.043 0.405-1.436 0l-7.492-7.724z"></path>
     </svg>
     <div class="dropdown-content" style="display: none;">
-      <a class="coveo-dropdown-item" data-label="All Prisma Cloud Documentation" data-value="@panproductcategory==('Prisma Cloud')">All Prisma Cloud Documentation</a>
-      <a class="coveo-dropdown-item selected" data-label="All Documentation" data-value="all">
-      All Documentation
+      <a href="#" class="coveo-dropdown-item" data-label="All Prisma Cloud Documentation" data-value="@panproductcategory==('Prisma Cloud')">All Prisma Cloud Documentation</a>
+      <a href="#" class="coveo-dropdown-item selected" data-label="All Documentation" data-value="all">
+        All Documentation
       </a>
     </div>
   </div>
